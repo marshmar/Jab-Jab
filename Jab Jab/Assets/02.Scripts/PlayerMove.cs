d5007d9f20c987695e123313cd063f2cdd0d9ee6
marshmar <goekazld123@naver.com>
2025-07-13 23:00:31 +0900

    feat: implement plaayer basic movement

---
+++ Jab Jab/Assets/02.Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody), typeof(PlayerInput))]
+public class PlayerMove : MonoBehaviour
+{
+    private PlayerInput _input;
+    private InputAction _moveAction;
+    private Rigidbody   _rigid;
+    private Vector3     _moveDir;
+    private float       _moveSpeed;
+
+    public float MoveSpeed { get => _moveSpeed; set => _moveSpeed = value; }
+
+    private void Awake()
+    {
+        _rigid = this.GetComponentSafe<Rigidbody>();
+        _input = this.GetComponentSafe<PlayerInput>();
+
+        InitializeMoveAction();
+    }
+
+    private void InitializeMoveAction()
+    {
+        _moveAction = _input.actions.FindAction("Move");
+
+        if (_moveAction.IsNull<InputAction>()) return;
+
+        _moveAction.performed += context =>
+        {
+            SetMoveDirection(context);
+        };
+
+        _moveAction.canceled += context =>
+        {
+            _moveDir = Vector3.zero;
+        };
+    }
+
+    private void SetMoveDirection(InputAction.CallbackContext context)
+    {
+        Vector2 contextVec = context.ReadValue<Vector2>();
+        _moveDir = new Vector3(contextVec.x, 0, contextVec.y);
+    }
+
+    private void Move()
+    {
+        _rigid.MovePosition(_rigid.position + _moveDir);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+}
